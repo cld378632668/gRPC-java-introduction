7648df39070de0371ca93b12265fc6ed8f8645bd
lidong.chen <378632668@qq.com>
2020-04-02 14:28:17 +0800

    complete client and server

--- src/main/java/GRPCSever.java
+++ src/main/java/GRPCSever.java
@@ -9,7 +9,7 @@ public class GRPCSever {
     public static void main(String[] args) throws IOException, InterruptedException {
         io.grpc.Server server = ServerBuilder.forPort(9090).addService(new UserService()).build();
         server.start();
-        System.out.println("Server started at" + server.getPort());
+        System.out.println("Server started at " + server.getPort());
         server.awaitTermination();
     }
 }
